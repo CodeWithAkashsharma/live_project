3f845ae778ba3717093cbb7b7a712724ce270d3b
akash sharma <122359568+DevWithAkash@users.noreply.github.com>
2025-11-23 17:17:54 +0530

    create folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
-// import AuthForm from './pages/AuthForm'
-import UserDash from './pages/UserDash'
+import AuthForm from './pages/AuthForm'
+// import UserDash from './pages/UserDash'
 // import AdminDash from './pages/AdminDash'
+
 function App() {
   
-
   return (
+    
     // <AdminDash></AdminDash>
-  <UserDash></UserDash>
-  //  <AuthForm> </AuthForm>
+  // <UserDash></UserDash>
+   <AuthForm> </AuthForm>
   )
 }
 
--- src/pages/UserDash.jsx
+++ src/pages/UserDash.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { User, Plus, FileText } from 'lucide-react';
 
-// Main Application Component
+
 const App = () => {
   // Mock User Data
   const userName = 'nandini sharma';
